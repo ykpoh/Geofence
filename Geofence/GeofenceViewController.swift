78b9f93ddf6f79109969cf687b8decbc352af644
YK <yungkien@myboost.com.my>
2021-03-24 21:54:24 +0800

    Add NavigationController and RootViewController with Maps

---
+++ Geofence/GeofenceViewController.swift
@@ -0,0 +1,13 @@
+//
+//  GeofenceViewController.swift
+//  Geofence
+//
+//  Created by Poh  Yung Kien on 24/03/2021.
+//
+
+import Foundation
+import UIKit
+
+class GeofenceViewController: UIViewController {
+    
+}
