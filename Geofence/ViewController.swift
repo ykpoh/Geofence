1f29aa5c08dd95fe67cb278f3f8f3f7dc701edca
YK <yungkien@myboost.com.my>
2021-03-24 20:55:33 +0800

    Initial Commit

---
+++ Geofence/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Geofence
+//
+//  Created by Poh  Yung Kien on 24/03/2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
