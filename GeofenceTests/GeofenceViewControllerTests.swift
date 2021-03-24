3eff0c1c8caeb9e5b5695f8387ddcb4b27386d3d
YK <yungkien@myboost.com.my>
2021-03-24 22:24:31 +0800

    Add GeofenceViewControllerTests

--- GeofenceTests/GeofenceViewControllerTests.swift
+++ GeofenceTests/GeofenceViewControllerTests.swift
@@ -1,14 +1,14 @@
 //
-//  GeofenceTests.swift
+//  GeofenceViewControllerTests.swift
 //  GeofenceTests
 //
 //  Created by Poh  Yung Kien on 24/03/2021.
 //
 
 import XCTest
-@testable import Geofence
 
-class GeofenceTests: XCTestCase {
+class GeofenceViewControllerTests: XCTestCase {
+    
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
